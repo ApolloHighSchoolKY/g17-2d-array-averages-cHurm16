2c7a9da52eddf553118b6c64748faa677d70c36f
cHurm16 <chloe.hurm@stu.daviess.kyschools.us>
2025-02-12 14:22:43 +0000

    homework changes

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -8,46 +8,70 @@ public class ArrayAverages{
     ray = rayray;
   }
 
+  //int val=1;
   public void rowAverages(){
-    for (int row=0;row<ray.length;row++)
+    int rowSum =0;
+    double rowAvg=0.0;
+    /*for (int row=0;row<ray.length;row++)
     {
     for (int col=0; col<ray[row].length; col++)
     {
-      ray[row][col]=0;
+      ray[row][col]=val;
+      val++;*/
+      for (int row=0;row<ray.length;row++){
+        rowSum =0;
+      for (int col=0; col<ray[row].length; col++)
+      {
+        rowSum += ray[row][col];
     }
+      }
+      rowAvg = rowSum / ray[row].length;
+
+      System.out.println("The average of row " + row + "is: "+ rowAvg);
+
     }
-  }
+    
+  
 
   public void columnAverages()
   {
     //Column major order
     //  Take each column and go through all of the rows
     //  Top down and Left to Right
-    int colAvg =0;
-    for(int col=0; col<ray.length; col++)
+    int colSum=0;
+    double colAvg =0.0;
+    for(int col=0; col<ray[0].length; col++)
     {
+      colSum =0;
     for(int row=0; row<ray[col].length; row++)
     {
-      colAvg += col/row;
+      colSum += ray[row][col];
+      /*
+       * ray[0][0]
+       * ray[1][0]
+       * ray[2][0]
+       */
     }
+    colAvg= colSum/ ray.length;
     }
 
   }
 
   public void arrayAverage(){
-    for (int row =0; row<ray.length; row++)
-    for (int col=0; col<ray[row].length; col++)
-
-    {
-      ray[row][col] =(int)(Math.random()*11+ 0);
+    int arraySum=0;
+    double arrayAverage= 0.0;
+    for (int row=0;row<ray.length;row++){
+      
+    for (int col=0; col<ray[row].length; col++){
+      arraySum += ray[row][col];
     }
+    //          total   / (number of rows * the number of colums)
+    arrayAverage= arraySum / (ray.length * ray[0].length);
 
-    System.out.println("");
+    //System.out.println("The average of row " + row + "is: "+ rowAvg);
 
-    for (int row =0; row<ray.length; row++)
-    {
-      System.out.println(Arrays.toString(ray[row]));
-    }
+  }
+   
 
   }
 
